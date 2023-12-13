b0027d0e66dcfe8b0fa31b7c13ad5442b9d8b5c4
BuildTools <unconfigured@null.spigotmc.org>
2023-12-13 09:50:35 -0300

    `MessageResponseDTO` movido de Package

--- src/main/java/me/eokasta/appvirtualbank/user/UserController.java
+++ src/main/java/me/eokasta/appvirtualbank/user/UserController.java
@@ -1,6 +1,7 @@
 package me.eokasta.appvirtualbank.user;
 
 import jakarta.validation.Valid;
+import me.eokasta.appvirtualbank.utils.MessageResponseDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/user")
 public class UserController {
 
-    private UserRepository repository;
+    private final UserRepository repository;
 
     @Autowired
     public UserController(UserRepository repository) {
